34e445a7b758393a0fbcd79815fa40c1c15cc2a4
krox2 <developer-krox@yandex.ru>
2022-06-29 12:15:20 +0300

    290622-1230 Продолжение

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,8 @@
+@extends('templates.main')
+@section('main_content')
+    <div class="wrapper-content">
+      <div class="container">
+          <span class="title">Это главная страница</span>
+      </div>
+    </div>
+@endsection
---
+++ resources/views/templates/header.blade.php
@@ -0,0 +1,8 @@
+<div class="header-wrapper">
+{{--    img(:src='$store.state.contentHeader.logo' alt="logo")--}}
+    <img src="{{ asset('assets/images/logo_proxima.png') }}" alt="logo">
+    <div class="info">
+{{--        Telefon(:tel='$store.state.contentHeader.telefon' class="m-top")--}}
+{{--        span {{ $store.state.contentHeader.subtitle }}--}}
+    </div>
+</div>
